d5c83049ebc48b02ed00ce7d872265bb2d651aee
ifadahImran <ifadaimran@gmail.com>
2024-04-21 11:42:07 +0530

    trying for datatypes

---
+++ FirstWeek/data_Types.py
@@ -0,0 +1,38 @@
+#String
+x = "python{}"
+y = 'elephant'
+#docstring
+z = """\n Hello World
+can include paragraphs 
+"""
+
+#Print 
+#print(x,y,z)
+
+#to findout the data type
+#print(type (x))
+
+#to findout the data type with the output 
+#print(type (x),x)
+
+#print only the 0th index (first character)
+#print(x[0])
+
+#print only the one before last index 
+#print(x[-2])
+
+#INT
+a=0
+
+#to findout the data type
+#print(type (a))
+
+#concat
+#print(x,a)
+#print(x.format(a))
+#print(x+y+str(a))
+print(f"Ifadah {y}")
+
+#FLOAT
+b =0.0 
+print(type(b))
